c52acbab9dbb81b63532ebb2b355eabeb1569830
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2025-06-14 19:14:27 +0330

    feat: develop swagger for send otp route

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -4,13 +4,14 @@ const swaggerUi = require("swagger-ui-express")
 function SwaggerConfig(app){
     const swaggerDocument = swaggerJsDoc({
         swaggerDefinition: {
+        openapi: "3.0.1",
         info : {
             title: "Real Estate",
             description: "Real Estate Consulting",
             version: "1.0.0"
         }
         },
-        apis: []
+        apis: [process.cwd() + "/src/modules/**/*.swagger.js"]
     });
    const Swagger = swaggerUi.setup(swaggerDocument, {});
     app.use("/" , swaggerUi.serve , Swagger)
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,10 +1,9 @@
 const {Router} = require("express");
 const authController = require("./auth.controller");
-const { sendOTP, checkOTP } = require("./auth.service");
 const router = Router();
 
-router.post("/send_otp" , authController , sendOTP);
-router.post("/check_otp" , authController , checkOTP);
+router.post("/send_otp" , authController.sendOTP);
+router.post("/check_otp" , authController.checkOTP);
 
 module.exports = {
     AuthRouter: router
