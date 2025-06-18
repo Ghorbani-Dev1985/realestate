8588d920f26e6f33b7addacbf193a8b763d196c5
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2025-06-18 16:20:53 +0330

    feat: develop create function in post controller

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -5,6 +5,7 @@ const PostService = require("./post.service");
 const PostMessage = require("./post.message");
 const CategoryModel = require("./../category/category.model");
 const createHttpError = require("http-errors");
+const { Types } = require("mongoose");
 
 class PostController {
   #service;
@@ -17,25 +18,32 @@ class PostController {
     try {
         let {slug} = req.query;
         let match = {parent: null};
-        let options;
+        let options , category;
         if(slug){
           slug = slug.trim();
-          const childrenCategory = await CategoryModel.findOne({slug});
-          if(!childrenCategory) throw new createHttpError.NotFound(PostMessage.NotFoundCategory);
-          options = await this.#service.getCategoryOptions(childrenCategory.id);
+          category = await CategoryModel.findOne({slug});
+          if(!category) throw new createHttpError.NotFound(PostMessage.NotFoundCategory);
+          options = await this.#service.getCategoryOptions(category.id);
           if(options.length === 0) options = null;
           match = {parent: CategoryModel._id}
         }
         const categories = await CategoryModel.aggregate([{$match: match}])
-        return res.json(categories , options)
+        return res.json(categories , options , category?._id.toString())
     } catch (error) {
         next(error)
     }
   }
  async create(req , res , next) {
     try {
-        const {name , icon , slug , parent} = req.body;
-        await this.#service.create({name , icon , slug , parent});
+        const {title , description , category , lat , lng} = req.body;
+        delete req.body['post'];
+        delete req.body['description'];
+        delete req.body['category'];
+        delete req.body['lat'];
+        delete req.body['lng'];
+        delete req.body['images'];
+        const options = req.body;
+        await this.#service.create({title , description , category: new Types.ObjectId(category) , coordinate: [lat , lng] , images: [], options});
        return res.status(HttpCodes.CREATED).json({message: PostMessage.Created});
     } catch (error) {
         next(error);
--- src/modules/post/post.model.js
+++ src/modules/post/post.model.js
@@ -2,13 +2,14 @@ const { Schema, Types } = require("mongoose");
 
 const PostSchema = new Schema({
    title: {type: String , required: true},
-   content: {type: String , required: true},
+   description: {type: String , required: true},
    category: {type: Types.ObjectId, ref: "Category" , required: true},
-   province: {type: String , required: true},
-   city: {type: String , required: true},
-   district: {type: String , required: true},
+   province: {type: String , required: false},
+   city: {type: String , required: false},
+   district: {type: String , required: false},
    coordinate: {type: [Number], required: true},
-   images: {type: [String], required: false, default: []}
+   images: {type: [String], required: false, default: []},
+   options: {type: Object , default: {}}
 }, {timestamps: true});
 
 const PostModel = model("post" , PostSchema);
